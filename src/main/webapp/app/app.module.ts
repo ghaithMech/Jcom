6b479b63b7e233168c2fd76469c9b224c876c0b0
ghaith.mechmech <ghaith.mechmech@gmail.com>
2020-02-25 17:42:56 +0100

    menu

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -13,6 +13,7 @@ import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ErrorComponent } from './layouts/error/error.component';
+import { MenuComponent } from './layouts/menu/menu.component';
 
 @NgModule({
   imports: [
@@ -24,7 +25,7 @@ import { ErrorComponent } from './layouts/error/error.component';
     JcomEntityModule,
     JcomAppRoutingModule
   ],
-  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, FooterComponent],
+  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, FooterComponent, MenuComponent],
   bootstrap: [MainComponent]
 })
 export class JcomAppModule {}
